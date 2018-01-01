d35359ccf2edb2645494812784d3842df4ee967e
Bryce Matsuda <brycematsuda@gmail.com>
2017-12-31 23:04:28 -1000

    Cleanup comments in views.py

--- ddr/views.py
+++ ddr/views.py
@@ -49,16 +49,15 @@ def song_list_by_mode_level(request, mode, level):
   # Filter out Japanese songs and move them to the front of the list
   # (already sorted above provided sort name is in hiragana)
   alphanum = re.compile(r'^[A-Za-z0-9]+$')
-  # Can't use set() - set() like in song_list_by_folder because __song_list_mode_level_helper returns a dictionary
-  # so create two separate lists with opposite regexes
   jp_songs = list(filter(lambda x: not alphanum.match(x['sort_name'][0]), songs))
   alphanum_songs = list(filter(lambda x: alphanum.match(x['sort_name'][0]), songs))
   songs = jp_songs + alphanum_songs
 
   return render(request, 'songs/list_by_mode_level.html', {'mode': mode, 'level': level, 'songs': songs})
 
-# Returns list of songs given a mode, difficulty, and level
 def __song_list_mode_level_helper(mode, level, difficulty):
+  """Returns list of songs given a mode, difficulty, and level"""
+  
   if mode == 'double' and difficulty == 'beginner':
     return Song.objects.none() # double has no beginner charts
   else:    
